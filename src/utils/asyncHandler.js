939ed18ce3f6f60d60fa3acc645bd21d9d0f4185
turabhaider2023 <turabw@gmail.com>
2026-07-12 21:00:22 +0530

    completing at home ApiError,ApiResponse,asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHandler = (fn)=>{
+    return (req,res,next)=>{
+        
+            Promise
+            .resolve(fn(req,res,next))
+            .catch(next)
+    
+            
+        
+    }
+}
+
+export default asyncHandler;
